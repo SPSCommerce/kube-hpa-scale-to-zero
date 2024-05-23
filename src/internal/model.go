b3941b94c0adb3ca159ec070ccc7020eef16d489
Oleksandr Novak <onovak@spscommerce.com>
2024-05-23 16:53:45 +0200

    remove unused code (#28)

--- src/internal/model.go
+++ src/internal/model.go
@@ -1,27 +1,5 @@
 package internal
 
-type MetricValueList struct {
-	Kind       string `json:"kind"`
-	ApiVersion string `json:"apiVersion"`
-	Metadata   struct {
-		SelfLink string `json:"selfLink"`
-	} `json:"metadata"`
-	Items []struct {
-		DescribedObject struct {
-			Kind       string `json:"kind"`
-			Namespace  string `json:"namespace"`
-			Name       string `json:"name"`
-			ApiVersion string `json:"apiVersion"`
-		} `json:"describedObject"`
-		MetricName string `json:"metricName"`
-		Timestamp  string `json:"timestamp"`
-		Value      string `json:"value"`
-		Selector   struct {
-			MatchLabels map[string]string `json:"matchLabels"`
-		} `json:"selector"`
-	} `json:"items"`
-}
-
 type DeploymentReplicasSpec struct {
 	Replicas int `json:"replicas"`
 }
